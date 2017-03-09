6054c4f1358c2e18ee787c4fa2a83e6eac95081b
rdv_consulting <r.rodrigues78@yahoo.com.br>
2017-03-09 11:04:52 -0300

    Exercicios do Curso FJ-36 (Camel)

---
+++ src/br/com/caelum/camel/TesteRoteamento.java
@@ -0,0 +1,33 @@
+package br.com.caelum.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class TesteRoteamento {
+	public static void main(String[] args) throws Exception {
+
+		CamelContext context = new DefaultCamelContext();
+
+		context.addRoutes(new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+
+				errorHandler(deadLetterChannel("file:falha")
+						.useOriginalMessage().maximumRedeliveries(2)
+						.redeliveryDelay(2000)
+						.retryAttemptedLogLevel(LoggingLevel.ERROR));
+
+				from("file:entrada?delay=5s&noop=true").to(
+						"validator:file:xsd/pedido.xsd").to("file:saida");
+			}
+		});
+
+		context.start();
+		Thread.sleep(30 * 1000);
+		context.stop();
+
+	}
+}
